31895b52f5a1831a5e0bc98e24308e0cbecee4eb
hiep <hiep@rmail.com>
2021-08-22 23:37:46 +0700

    Update

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,56 @@
+
+public class Rectangle {
+    private double width, height;
+    String color;
+    
+    public Rectangle() {
+        this.width = 1;
+        this.height = 1;
+    }
+    
+    public Rectangle(double a, double b) {
+        this.width = a;
+        this.height = b;
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public void setWidth(double a) {
+        this.width = a;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public void setHeight(double a) {
+        this.height = a;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+    
+    public double findArea() {
+        return this.height * this.width;
+    }
+    
+    public double findPerimeter() {
+        return 2*(this.height + this.width);
+    }
+    
+    public static void main(String[] args) {
+        Rectangle a = new Rectangle(5, 6);
+        a.setColor("Red");
+        System.out.println("Màu s?c: " + a.getColor());
+        System.out.println("Di?n tích: " + a.findArea());
+        System.out.println("Chu vi: " + a.findPerimeter());
+    }
+    
+}
