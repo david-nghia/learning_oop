0e1550bd6c4cb809501e82a9e13455e4f560ea5e
Duy Pham <duypham@email.com>
2021-08-22 21:24:26 +0700

    init project

---
+++ src/PhanSo.java
@@ -0,0 +1,17 @@
+public class PhanSo {
+    int tu, mau;
+
+    public PhanSo(){
+//        System.out.println("Init method");
+        tu = 0;
+        mau = 1;
+    }
+
+
+
+
+    void value(){
+        System.out.println("Value = " + (float)tu/mau);
+    }
+
+}
