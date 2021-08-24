f8c5363859ca180241356135d0afd2aa81e672d8
hiep <hiep@rmail.com>
2021-08-24 20:59:05 +0700

    update ra vao file

--- src/main/java/fileio/SortCollections.java
+++ src/main/java/fileio/SortCollections.java
@@ -7,7 +7,7 @@ public class SortCollections {
 
     private PrintWriter output;
     private int[] array;
-
+    private int n;
     public SortCollections(String filename) throws FileNotFoundException {
         readData(filename);
         this.output = new PrintWriter(new File("output.txt"));
@@ -16,7 +16,8 @@ public class SortCollections {
 
     private void readData(String filename) throws FileNotFoundException {
         Scanner input = new Scanner(new File(filename));
-        array = new int[100];
+        n = input.nextInt();
+        array = new int[n];
         for (int i = 0; i < 10; i++) {
             array[i] = input.nextInt();
         }
@@ -30,13 +31,14 @@ public class SortCollections {
         output.println();
     }
 
-    public void selectionSort() {
-        int[] ar = new int[100];
-        System.arraycopy(array, 0, ar, 0, 10);
-        for (int i = 0; i < 10; i++) {
+    public int[] selectionSort() {
+        int size = array.length;
+        int[] ar = new int[size];
+        System.arraycopy(array, 0, ar, 0, size);
+        for (int i = 0; i < size; i++) {
             int index = i;
             int min = ar[i];
-            for (int j = i; j < 10; j++) {
+            for (int j = i; j < size; j++) {
                 if (ar[j] < min) {
                     min = ar[j];
                     index = j;
@@ -48,14 +50,15 @@ public class SortCollections {
                 ar[index] = tmp;
                 //System.out.println(index);
             }
-            display(ar);
         }
+        return ar;
     }
 
-    public void inserttionSort() {
-        int[] ar = new int[100];
-        System.arraycopy(array, 0, ar, 0, 10);
-        for (int i = 1; i < 10; i++) {
+    public int[] inserttionSort() {
+        int size = array.length;
+        int[] ar = new int[size];
+        System.arraycopy(array, 0, ar, 0, size);
+        for (int i = 1; i < size; i++) {
             int key = ar[i];
             int j = i - 1;
             while (j >= 0 && ar[j] > key) {
@@ -63,28 +66,30 @@ public class SortCollections {
                 j--;
             }
             ar[j + 1] = key;
-            display(ar);
         }
+        return ar;
     }
 
-    public void bubbleSort() {
-        int[] ar = new int[100];
-        System.arraycopy(array, 0, ar, 0, 10);
+    public int[] bubbleSort() {
+        int size = array.length;
+        int[] ar = new int[size];
+        System.arraycopy(array, 0, ar, 0, size);
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 9 - i; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
                 if (ar[j] > ar[j + 1]) {
                     int tmp = ar[j];
                     ar[j] = ar[j + 1];
                     ar[j + 1] = tmp;
                 }
             }
-            display(ar);
         }
+        return ar;
     }
 
     public int search(int x) {
-        for (int i = 0; i < 10; i++) {
+        int size = this.n;
+        for (int i = 0; i < size; i++) {
             if (this.array[i] == x) {
                 return i;
             }
@@ -93,8 +98,9 @@ public class SortCollections {
     }
 
     public int binarySearch(int x) {
+        int size = this.array.length;
         int l = 0;
-        int r = 9;
+        int r = size - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
             if (this.array[mid] == x) {
@@ -111,12 +117,19 @@ public class SortCollections {
     public static void main(String[] args) throws FileNotFoundException {
         String filename = "DAYSO.txt";
         SortCollections sc = new SortCollections(filename);
-        sc.output.println("Quy trinh sap xep noi boi: ");
-        sc.bubbleSort();
+        int[] res = new int[sc.n];
+        
+        res = sc.bubbleSort();
+        sc.output.println("sap xep noi boi: ");
+        sc.display(res);
+        
+        res = sc.selectionSort();
         sc.output.println("Quy trinh sap xep chon: ");
-        sc.selectionSort();
+        sc.display(res);
+        
+        res = sc.inserttionSort();
         sc.output.println("Quy trinh sap xep chen: ");
-        sc.inserttionSort();
+        sc.display(res);
         
         sc.output.close();
     }
